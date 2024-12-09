802603a0cce28baf173ebec1815e13b15fdc9899
sojung519 <thwjd990519@gmail.com>
2024-12-09 03:14:40 +0000

    update model_FLCAM

--- utils/model_FLCAM.py
+++ utils/model_FLCAM.py
@@ -85,8 +85,6 @@ class CNN(nn.Module):
                  n_filt=[64, 64, 64],
                  pooling=[(1, 4), (1, 4), (1, 4)],
                  normalization="batch",
-                 n_basis_kernels=4,
-                 DY_layers=[0, 1, 1, 1, 1, 1, 1],
                  atte_layers=[0, 1, 1, 1, 1, 1, 1],
                  temperature=31,
                  pool_dim='time'):
