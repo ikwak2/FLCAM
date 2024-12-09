10b3f03e931199911c341a2369fbb6045c4e56dc
sojung519 <thwjd990519@gmail.com>
2024-12-09 03:13:51 +0000

    update exp5

--- utils/exp5_model_FBAM.py
+++ utils/exp5_model_FBAM.py
@@ -85,8 +85,6 @@ class CNN(nn.Module):
                  n_filt=[64, 64, 64],
                  pooling=[(1, 4), (1, 4), (1, 4)],
                  normalization="batch",
-                 n_basis_kernels=4,
-                 DY_layers=[0, 1, 1, 1, 1, 1, 1],
                  atte_layers=[0, 1, 1, 1, 1, 1, 0],
                  freqdim=[128, 64, 32, 16, 8, 4, 2],
                  temperature=31,
